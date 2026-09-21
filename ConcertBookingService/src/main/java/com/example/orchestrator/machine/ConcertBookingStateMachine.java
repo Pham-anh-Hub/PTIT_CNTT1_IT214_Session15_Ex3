0ec84fce3ea0aba434c20c0f2555dbcdc52fb0dd
Phạm Phương Anh <phamphuonganhcutee@gmail.com>
2026-09-21 22:39:03 +0700

    Save local changes before pull

--- ConcertBookingService/src/main/java/com/example/orchestrator/machine/ConcertBookingStateMachine.java
+++
@@ -1,7 +0,0 @@
-package com.example.orchestrator.machine;
-
-import com.example.orchestrator.model.BookingTransaction;
-
-public interface ConcertBookingStateMachine {
-    void process(BookingTransaction transaction);
-}
